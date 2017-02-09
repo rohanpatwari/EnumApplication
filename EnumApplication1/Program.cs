318c1d9d8676f9f9a6a3642f5461b81f633361b8
rohan <rohan@DESKTOP-2911EVS>
2017-02-09 15:35:37 -0600

    Add project files.

---
+++ EnumApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumApplication1
+{
+    public class Employee
+    {
+        public string FirstName;
+        public string LastName;
+
+    }
+    enum WorksIn { Google, Yahoo, Ebay, Apple, Facebook, IBM };
+    public class program
+    { 
+        public static void Main(string[] args)
+        {
+            Employee Emp = new Employee();
+            string FullName;
+            Console.WriteLine("List Of Companies:");
+            foreach (string str in Enum.GetNames(typeof(WorksIn)))
+            Console.WriteLine(str);
+            Console.WriteLine("Enter the First Name: ");
+            Emp.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name: ");
+            Emp.LastName = Console.ReadLine();
+            Console.WriteLine("Enter the Company Name:");
+            WorksIn Company = (WorksIn)(Enum.Parse(typeof(WorksIn),Console.ReadLine()));
+            FullName = Emp.FirstName + " " + Emp.LastName;
+            Console.WriteLine("Full Name && Works In: {0},{1}",FullName, Company);
+            Console.ReadLine();
+        }
+    }
+}
